fdf6d449bc88249fedfef7932553afc36ee93082
arielfc <48219012+arielfc@users.noreply.github.com>
2019-06-10 14:53:12 -0600

    Initial commit with DB architecture, web project layout and site mapping
    Setup of remote DB using azure, site map layout and web project creation with proper dependencies

---
+++ TravelExpertsWeb/TravelExpertsWeb/Models/Affiliations.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravelExpertsWeb.Models
+{
+    public partial class Affiliations
+    {
+        public string AffilitationId { get; set; }
+        public string AffName { get; set; }
+        public string AffDesc { get; set; }
+    }
+}
---
+++ TravelExpertsWeb/TravelExpertsWeb/Models/Rewards.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TravelExpertsWeb.Models
+{
+    public partial class Rewards
+    {
+        public int RewardId { get; set; }
+        public string RwdName { get; set; }
+        public string RwdDesc { get; set; }
+    }
+}
