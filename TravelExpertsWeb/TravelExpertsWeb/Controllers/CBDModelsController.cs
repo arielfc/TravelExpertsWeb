0d95439229f538a9f6467a1e1eba6d8b2d16e342
Barbara1903 <barbarayyang@hotmail.com>
2019-06-13 23:06:10 -0600

    Add a combined Model View Controller (CBDModel) Change Packages/Index

---
+++ TravelExpertsWeb/TravelExpertsWeb/Controllers/CBDModelsController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using TravelExpertsWeb.Models;
+
+namespace TravelExpertsWeb.Controllers
+{
+    public class CBDModelsController : Controller
+    {
+        private readonly TravelExpertsDBContext _context;//TravelExpertsContext
+
+        public CBDModelsController(TravelExpertsDBContext context)//TravelExpertsContext
+        {
+            _context = context;
+        }
+
+        public ActionResult Index(int? id)
+        {
+            CBDModels results = new CBDModels();
+            //var temp = from c1 in _context.Customers
+            //                    where c1.CustomerId == id
+            //                    select c1;
+            results.customer = _context.Customers.FirstOrDefault(c => c.CustomerId == id);
+            results.bookingDetails = from c in _context.BookingDetails.Include(c => c.Fee)
+                                     join b in
+                                       from b in _context.Bookings
+                                       where b.CustomerId == id
+                                       select b
+                                     on c.BookingId equals b.BookingId
+                                     join f in _context.Fees
+                                     on c.FeeId equals f.FeeId
+                                     orderby c.TripStart
+                                     select c;
+            ViewBag.id = id;
+            return View(results);
+        }
+
+    }
+}
---
+++ TravelExpertsWeb/TravelExpertsWeb/Models/CBDModels.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TravelExpertsWeb.Models
+{
+    public class CBDModels
+    {
+        public IEnumerable<BookingDetails> bookingDetails { get; set; }
+        public Customers customer { get; set; }
+    }
+}
